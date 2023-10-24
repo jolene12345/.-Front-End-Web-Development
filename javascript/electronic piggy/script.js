a6eb386eacf156bcdc1f79e103e5d38f5302d93e
jolene12345 <jolliu26@bergen.org>
2023-10-24 12:18:20 -0400

    fin

---
+++ javascript/electronic piggy/script.js
@@ -0,0 +1,52 @@
+/**
+
+Electronic Piggy Bank! (But now as a static webpage!)
+
+Your little brother Timmy is back at it again with the electronic piggy banks. 
+Now it is 2023 so Timmy doesn't want to run a Java program to enter in his money. Instead, he prefers a website!
+
+Functions to consider using:
+
+1. Get an HTML element using its id
+    let htmlElement = document.getElementById("id");
+
+2. Adding an event listener for click:
+    htmlElement.addEventListener("click", function)
+
+3. Getting the user's input text:
+    [The name of your input variable].value
+
+Some other helpful functions:
+str.toLowerCase() //turns the string str into lowercase 
+.toFixed(2) // rounds a number to 2 decimal places
+ */
+
+let piggyBankAmount = 0.0;
+let button = document.getElementById("coinEnter");
+let input = document.getElementById("coinInput");
+
+button.addEventListener("click", function f() {
+    const coinType = input.value.toLowerCase();
+
+    if(coinType === "p"){
+        piggyBankAmount += .01;
+    }
+    else if(coinType === "n"){
+        piggyBankAmount += .05;
+    }
+    else if(coinType === "d"){
+        piggyBankAmount += .10;
+    }
+    else if(coinType === "q"){
+        piggyBankAmount += .25;
+    }
+
+    
+    input.value = "";
+    updatePiggyBankAmount();
+});
+
+function updatePiggyBankAmount(){
+    document.getElementById("piggyBank").textContent = "$" + piggyBankAmount.toFixed(2)
+    // .toFixed(x) rounds number to x amount of digits
+}
---
+++ javascript/sandbox.js
@@ -0,0 +1,40 @@
+// let age = 15;
+// let year = 2023;
+
+// console.log(age, year)
+
+// let firstName = "Jolene"
+// let lastName = "Liu"
+
+// let fullName = firstName + " " + lastName;
+
+// console.log(fullName)
+// console.log(fullName.length)
+// console.log(fullName[3])
+// console.log(fullName.toUpperCase())
+
+// let date = 17;
+// if(date === "17"){
+//     console.log("yey")
+// }
+
+// function add(a, b){
+//     return a+b
+// }
+
+// let answer = add(1,2)
+// console.log(answer)
+
+// function printHi(name){
+//     return "hi " + name
+// }
+
+// function printBye(func){
+//     return func("jolene") + "bye"
+// }
+
+// console.log(printBye(printHi))
+
+
+const add = (a, b) => (a+b);
+console.log(add(5,8));
